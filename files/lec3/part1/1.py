560481dda36fb72383fac91f9190b1d02385b28e
TrickyWhiteCat <87902975+TrickyWhiteCat@users.noreply.github.com>
2022-04-26 15:24:36 +0700

    Update 1.py

--- files/lec3/part1/1.py
+++ files/lec3/part1/1.py
@@ -1,11 +1,5 @@
-def sum_of_div(a):
-    res = 0
-    for i in range(1, a):
-        if a % i == 0:
-            res += i
-    return res
-
 def friendly(a, b):
-    if a == sum_of_div(b) and b == sum_of_div(a):
+    if a == sum(i for i in range(1, b) if b % i == 0) and b == sum(i for i in range(1, a) if a % i == 0):
         return 'YES'
-    return 'NO'
+    else:
+        return 'NO'
