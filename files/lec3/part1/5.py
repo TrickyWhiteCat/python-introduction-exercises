4d5f873b97afa452b2bdb99a67c44a6de4590a83
TrickyWhiteCat <87902975+TrickyWhiteCat@users.noreply.github.com>
2022-04-26 15:30:10 +0700

    Update 5.py

--- files/lec3/part1/5.py
+++ files/lec3/part1/5.py
@@ -1,7 +1,6 @@
 def floyd(n):
-    current = '1'
-    s = ''
-    for i in range(n):
-        s = current + s
-        current = str(abs(int(current)-1))
-        print(s)
+    for i in range(1, n+1):
+        print(''.join([str((i - j)%2) for j in range(i)]))
+        
+# Debug: 0
+def floyd(n): print('\n'.join([''.join([str((i - j)%2) for j in range(i)]) for i in range(1, n+1)]))
