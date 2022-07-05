69fc05a6d5cceab2550a97ad4566bc64ca2121a8
TrickyWhiteCat <87902975+TrickyWhiteCat@users.noreply.github.com>
2022-07-05 22:09:32 +0700

    Basically changed everything

--- files/lec8/part1/7.py
+++ files/lec8/part1/7.py
@@ -1,32 +1,17 @@
-class Matrix(object):
-    def __init__(self, lst):
-        self.lst = lst
+class Matrix:
+    def __init__(self, rows):
+        self._rows = rows
+        self._num_rows = len(rows)
+        cols = []
+        for i, _ in enumerate(rows[0]):
+            cols.append([row[i] for row in rows])
+        self._cols = cols
+        self._num_cols = len(cols)
     
-    def size(self):
-        return (len(self.lst), len(self.lst[0]))
-        #     (number of rows, number of columns)
-        
-    def __str__(self):
-        s = ''
-        num_row, num_col = self.size()
-        
-        for row_ind in range(num_row):
-            if row_ind != 0:
-                s += '\n'
-            for col_ind in range(num_col):
-                s += str(self.lst[row_ind][col_ind])
-                if col_ind < num_col - 1:
-                    s += '\t'
-        return s    
-    
-'''  
-m = Matrix([[1, 0], [0, 1]])
-print(m)
-print(m.size())
-m = Matrix([[2, 0, 0], [0, 1, 10000]])
-print(m)
-print(m.size())
-m = Matrix([[-10, 20, 50, 2443], [-5235, 12, 4324, 4234]])
-print(m)
-print(m.size())
-'''
+    def size(self) -> tuple:
+        return (self._num_rows, self._num_cols)
+
+    def __str__(self) -> str:
+        for row in self._rows:
+            temp = '\t'.join(str(elem) for elem in row)
+        return '\n'.join('\t'.join(str(elem) for elem in row) for row in self._rows)
